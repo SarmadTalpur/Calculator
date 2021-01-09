9594b74bc8b0fa23e4e46164dd8e4b09170f9171
Sarmad <63968434+SarmadTalpur@users.noreply.github.com>
2021-01-09 16:01:59 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,22 +9,21 @@ class Calc:
         self.master = master
 
         # variables to get value from widgets
-        self.input_variable1 = tk.DoubleVar(master)
-        self.input_variable2 = tk.DoubleVar(master)
-        self.format_value = tk.IntVar(master)
-        self.result_value = tk.StringVar(master)
-        self.calc_result1 = tk.IntVar(master)
-        self.calc_result2 = tk.IntVar(master)
+        self.input_variable1 = tk.DoubleVar(master, 0.0)
+        self.input_variable2 = tk.DoubleVar(master, 0.0)
+        self.format_value = tk.IntVar(master, 0)
+        self.calc_result1 = tk.IntVar(master, 0)
+        self.calc_result2 = tk.IntVar(master, 0)
 
-        # using trace method to perform an action through calculate method when
-        # any of the below variable's value is changed
-        self.input_variable1.trace("w", self.calculate)
-        self.input_variable2.trace("w", self.calculate)
-        self.format_value.trace("w", self.calculate)
-
-        self.header1 = tk.Label(self.master, text="************************************", padx=20)
-        self.header2 = tk.Label(self.master, text="Created by Sarmad Talpur", padx=20)
-        self.header3 = tk.Label(self.master, text="************************************", padx=20)
+        self.header1 = tk.Label(
+            self.master, text="************************************", padx=20
+        )
+        self.header2 = tk.Label(
+            self.master, text="Created by Sarmad Talpur", padx=20
+        )
+        self.header3 = tk.Label(
+            self.master, text="************************************", padx=20
+        )
         self.header1.pack()
         self.header2.pack()
         self.header3.pack()
@@ -36,12 +35,17 @@ class Calc:
         # creating widgets for inputs
         self.label1 = tk.Label(input_frame, text="Enter first number").grid(
             row=0, column=0, padx=(10, 10))
-        self.entry1 = tk.Entry(input_frame, textvariable=self.input_variable1).grid(
-            row=0, column=1, padx=(10, 10))
+        self.entry1 = tk.Entry(input_frame, textvariable=self.input_variable1)
+        # binding an event to entry1
+        self.entry1.bind("<Return>", self.calculate)
+        self.entry1.grid(row=0, column=1, padx=(10, 10))
+
         self.label2 = tk.Label(input_frame, text="Enter second number").grid(
             row=1, column=0, padx=(10, 10))
-        self.entry2 = tk.Entry(input_frame, textvariable=self.input_variable2).grid(
-            row=1, column=1, padx=(10, 10))
+        self.entry2 = tk.Entry(input_frame, textvariable=self.input_variable2)
+        # binding an event to entry2
+        self.entry2.bind("<Return>", self.calculate)
+        self.entry2.grid(row=1, column=1, padx=(10, 10))
 
         # creating a frame for radio buttons that fits master frame
         format_frame = tk.Frame(self.master)
@@ -64,27 +68,29 @@ class Calc:
             format_frame, variable=self.format_value, value=3, text="Divide"
         ).grid(row=0, column=3)
 
+        # creating a button to calculate/solve the calculations
+        self.calc_button = tk.Button(
+            self.master, text="Solve", command=self.calculate
+        )
+        self.calc_button.bind("<Return>", self.calculate)
+        self.calc_button.pack()
+
         # creating a label for result
-        self.result = tk.Label(self.master, pady=10, textvariable=self.result_value)
+        self.result = tk.Label(self.master, pady=10)
         self.result.pack()
 
-        # method to quit application when Exit button is clicked
-        def end_calc():
-            exit()
-
         # creating a button to exit program
-        self.quit = tk.Button(self.master, text="Exit", padx=30, pady=8, command=end_calc)
+        self.quit = tk.Button(
+            self.master, text="Exit", padx=30, pady=8, command=self.end_calc)
+        self.quit.bind("<Return>", self.end_calc)
         self.quit.pack()
-
-        # calling method calculate
-        self.calculate()
 
     # to make a loop so application runs until events occur
     def start(self):
         self.master.mainloop()
 
-    # calculate method working definition
-    def calculate(self, *args):
+    # "calculate", method working definition
+    def calculate(self, event=None):
 
         # try if the values entered are numbers
         try:
@@ -93,18 +99,22 @@ class Calc:
 
         # or throw an error message
         except tk.TclError:
-            self.result_value.set("Invalid input!")
+            self.result["text"] = "Invalid input!"
 
         # if numbers are correct access this code
         else:
             if self.format_value.get() == 0:
-                self.result_value.set("Result: "+f"{round(self.calc_result1+self.calc_result2, 2)}")
+                self.result["text"] = "Result: " + f"{round(self.calc_result1+self.calc_result2, 2)}"
             elif self.format_value.get() == 1:
-                self.result_value.set("Result: "+f"{round(self.calc_result1-self.calc_result2, 2)}")
+                self.result["text"] = "Result: " + f"{round(self.calc_result1-self.calc_result2, 2)}"
             elif self.format_value.get() == 2:
-                self.result_value.set("Result: "+f"{round(self.calc_result1*self.calc_result2, 2)}")
+                self.result["text"] = "Result: "+f"{round(self.calc_result1*self.calc_result2, 2)}"
             else:
-                self.result_value.set("Result: "+f"{round(self.calc_result1/self.calc_result2, 2)}")
+                self.result["text"] = "Result: "+f"{round(self.calc_result1/self.calc_result2, 2)}"
+
+    # method to quit application when Exit button is clicked
+    def end_calc(self, event=None):
+        exit()
 
 
 if __name__ == '__main__':
